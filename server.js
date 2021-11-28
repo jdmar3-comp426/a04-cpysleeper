0b0364bd90c3d3b813c84196a954485df47d799e
cpysleeper <CPY943809389cpy>
2021-11-27 20:45:45 -0500

    s

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {
-	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').run(req.params.user, md5
-		(req.params.pass));
+	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').run(req.body.user, md5(req.body.pass));
 	res.status(200).json({"message":"1 record createed: ID 3 (201)"});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -48,12 +47,12 @@ app.get("/app/user/:id", (req, res) => {
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:idp
 app.patch("/app/update/user/:idp", (req, res)=> {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.params.user, md5(req.params.pass));
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.boby.user, md5(req.body.pass), req.params.id);
 	res.status(200).json({"message":"1 record updated: ID 2 (200)"})
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/suer/:id", (req, res)=> {
-	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.body.user, md5(req.body.pass));
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
 	res.status(200).json({"message":"1 record deleted: ID 2 (200)"})
 });
 // Default response for any other request
